23d9b7de95a57c39ca71ff5682f0dd1b06439524
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-19 14:17:10 +1100

    Add build dependencies for webdev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "Downloading dependencies & running app with 'flutter run'."
-cd /app && dart pub get && flutter run -d web-server --web-hostname 0.0.0.0 --web-port 8080
+cd /app && flutter pub get && flutter run --verbose -d web-server --web-hostname 0.0.0.0 --web-port 8080

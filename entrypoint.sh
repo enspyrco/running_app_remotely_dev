2b1912a23ea8d8f3a25392521c9779b1b753f2f0
Nick Meinhold <nick@enspyr.co>
2022-01-14 15:28:28 +1100

    Add flutter config --enable-web

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
-echo "Downloading dependencies & running app with 'flutter run'."
+flutter config --enable-web
+echo "Downloading dependencies & running app."
 cd /app && flutter pub get && flutter run --verbose -d web-server --web-hostname 0.0.0.0 --web-port 8080
